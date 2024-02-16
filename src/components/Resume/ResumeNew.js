de28c8ac89beb7d4794802cd17a8669bef732499
Surbhi Haldar <haldarsurbhi@gmail.com>
2024-02-16 20:44:49 +0530

    resume section change

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,7 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-function ResumeNew({forwardedRef}) {
+function ResumeNew({ forwardedRef }) {
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
@@ -19,35 +19,40 @@ function ResumeNew({forwardedRef}) {
     <div>
       <Container fluid className="resume-section" ref={forwardedRef}>
         <Particle />
+
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
-        <Button
-          variant="primary"
-          href={pdf}
-          download="surbhi-haldar-resume.pdf" // Add this attribute to specify the filename
-          style={{ maxWidth: "250px" }}
-        >
-          <AiOutlineDownload />
-          &nbsp;Download CV
-        </Button>
+          {/* <Button
+            variant="primary"
+            href={pdf}
+            download="surbhi-haldar-resume.pdf" // Add this attribute to specify the filename
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button> */}
+
+          <h1 style={{ color: "white", fontWeight: "900", marginBottom: "2%" }}>Get my Resume from here</h1>
         </Row>
 
-        <Row className="resume">
+        {/* <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} scale={width > 500 ? 1.7 : 0.6} />
           </Document>
-        </Row>
+        </Row> */}
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
-        <Button
-          variant="primary"
-          href={pdf}
-          download="surbhi-haldar-resume.pdf" // Add this attribute to specify the filename
-          style={{ maxWidth: "250px" }}
-        >
-          <AiOutlineDownload />
-          &nbsp;Download CV
-        </Button>
+          <Button
+            variant="primary"
+            href={pdf}
+            download="surbhi-haldar-resume.pdf" // Add this attribute to specify the filename
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
         </Row>
+        <div style={{ height: "20vh" }}></div>
       </Container>
     </div>
   );
